5f04d862266ab048800aa86c25a5674f2b48fd94
王浩锐 <1905811497@qq.com>
2023-02-07 17:59:12 +0800

    [what][test][SDL] 完善 SDL 测试用例

--- Sample/main.cpp
+++ Sample/main.cpp
@@ -13,14 +13,7 @@ int main()
 {
     // LogTest();
     // WindowsEGLTest();
-
-    InitLogger();
-    DisplaySDL displaySDL;
-    displaySDL.Init();
-    displaySDL.Open(1920, 1080);
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    displaySDL.Close();
-    displaySDL.UnInit();
+    DisplaySDLTest();
 
     return 0;
 }
